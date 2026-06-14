97e83c4c79fbba56edb2a9e9212c9b6be0df9a73
Zubair Quraishi <zubairq@gmail.com>
2026-06-14 12:42:13 +0200

    Read the DB structure

--- test.js
+++ test.js
@@ -12,14 +12,14 @@ let ret = noaccess.getTables()
 
 
 
-//let tablePages = noaccess.getTablePages()
-//console.log("Table pages: " + JSON.stringify(tablePages,null,2))
+let tablePages = noaccess.getTablePages()
+console.log("Table pages: " + JSON.stringify(tablePages,null,2))
 
-//console.log(JSON.stringify(ret,null,2))
+console.log(JSON.stringify(ret,null,2))
 
-let cols = noaccess.getColumns("CustomerT")
-console.log(JSON.stringify(cols,null,2))
+//let cols = noaccess.getColumns("Contact")
+//console.log(JSON.stringify(cols,null,2))
 
 
-let tableData = noaccess.getTableData("CustomerT")
-console.log(JSON.stringify(tableData,null,2))
+//let tableData = noaccess.getTableData("CustomerT")
+//console.log(JSON.stringify(tableData,null,2))
